555fa5d1e46d4814da04067b15cd2dcdd03ebe6a
mohamedhamouda7897 <mohamedhamouda.mahmoud@gmail.com>
2024-12-14 13:44:34 +0200

    theming & caching

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:islami_c13_str/cache/cache_helper.dart';
 import 'package:islami_c13_str/home/home.dart';
+import 'package:islami_c13_str/my_theme_data.dart';
 import 'package:islami_c13_str/onBoarding/onboarding.dart';
 import 'package:islami_c13_str/sura_details/sura_details.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await CacheHelper.init();
   runApp(const MyApp());
 }
 
@@ -13,10 +17,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: MyThemeData.lightTheme,
+      darkTheme: MyThemeData.darkTheme,
+      themeMode: ThemeMode.light,
       debugShowCheckedModeBanner: false,
-      initialRoute: OnboardingScreen.routeName,
+      initialRoute: CacheHelper.getEligibility() == true
+          ? HomeScreen.routeName
+          : OnboardingScreen.routeName,
       routes: {
-        SuraDetailsScreen.routeName: (context) =>  SuraDetailsScreen(),
+        SuraDetailsScreen.routeName: (context) => SuraDetailsScreen(),
         HomeScreen.routeName: (context) => HomeScreen(),
         OnboardingScreen.routeName: (context) => const OnboardingScreen(),
       },
---
+++ lib/my_theme_data.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MyThemeData {
+  static const Color primaryColor = Color(0xFFE2BE7F);
+  static const Color blackColor = Color(0xFF202020);
+  static ThemeData lightTheme = ThemeData(
+    scaffoldBackgroundColor: Colors.transparent,
+    appBarTheme: AppBarTheme(
+      centerTitle: true,
+      titleTextStyle: GoogleFonts.aBeeZee(fontSize: 20, color: primaryColor),
+      iconTheme: IconThemeData(
+        color: primaryColor,
+      ),
+    ),
+    textTheme: TextTheme(
+      titleSmall: GoogleFonts.elMessiri(
+        fontSize: 16,
+        fontWeight: FontWeight.w400,
+        color: primaryColor,
+      ),
+      titleMedium: GoogleFonts.elMessiri(
+        fontSize: 22,
+        fontWeight: FontWeight.bold,
+        color: primaryColor,
+      ),
+      titleLarge: GoogleFonts.elMessiri(
+        fontSize: 30,
+        fontWeight: FontWeight.bold,
+        color: primaryColor,
+      ),
+      bodySmall: GoogleFonts.exo2(
+        fontSize: 16,
+        fontWeight: FontWeight.w400,
+        color: primaryColor,
+      ),
+      bodyMedium: GoogleFonts.exo2(
+        fontSize: 22,
+        fontWeight: FontWeight.bold,
+        color: primaryColor,
+      ),
+      bodyLarge: GoogleFonts.exo2(
+        fontSize: 30,
+        fontWeight: FontWeight.bold,
+        color: primaryColor,
+      ),
+    ),
+    bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+      backgroundColor: primaryColor,
+      type: BottomNavigationBarType.fixed,
+      showUnselectedLabels: false,
+      selectedItemColor: Colors.white,
+      unselectedItemColor: blackColor,
+    ),
+  );
+  static ThemeData darkTheme = ThemeData();
+}
